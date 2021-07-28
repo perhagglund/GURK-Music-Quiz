8ee9d8f20d6c7f6d1769db5fd4ddf84ad026d09b
perhagglund <86194556+perhagglund@users.noreply.github.com>
2021-07-28 21:39:24 +0200

    namn header

--- frontend/src/components/NameField.js
+++ frontend/src/components/NameField.js
@@ -3,6 +3,7 @@ import React from "react";
 const NameField = () => {
     return (
         <div>
+            <h2 className={"nameHeader"}>Name</h2>
             <input type={"text"} placeholder={"Write in your name"}/>
         </div>
     )
