760d61f37ab0e0ceb922501e840488228e71afab
AdamBeijar <56826955+AdamBeijar@users.noreply.github.com>
2021-07-29 23:17:57 +0300

    Added-Start-Of-Lobby-Page

---
+++ frontend/lobbyPageSrc/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const App = () => {
+
+    return (
+        <div className={"body"}>
+            This is the Lobby, hells yeah
+        </div>
+    )
+}
+
+
+export default App
