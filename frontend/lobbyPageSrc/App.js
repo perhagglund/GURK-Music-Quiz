3071dd37b1193bfabdae29c3b9f31493a3f17996
AdamBeijar <56826955+AdamBeijar@users.noreply.github.com>
2021-07-30 15:11:01 +0300

    Added-randomizer-to-create-game-button

--- frontend/lobbyPageSrc/App.js
+++ frontend/lobbyPageSrc/App.js
@@ -2,9 +2,10 @@ import React from "react";
 
 
 const App = () => {
-
+    const roomName = window.location.pathname
     return (
         <div className={"body"}>
+            {roomName}
             This is the Lobby, hells yeah
         </div>
     )
