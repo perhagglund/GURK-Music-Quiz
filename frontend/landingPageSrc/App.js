a7a70b22e1531abc26e599ce397ecb8d86caa9a8
perhagglund <86194556+perhagglund@users.noreply.github.com>
2021-08-03 17:22:20 +0200

    added final cookiebanner style

--- frontend/landingPageSrc/App.js
+++ frontend/landingPageSrc/App.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import Form from "./components/Form";
 import CookieBanner from "./components/CookieBanner";
 
 
 const App = () => {
     document.title = "Musikquiz"
+
+    const [showCookieBanner, setShowCookieBanner] = useState('')
+
+    const changeVisibility = () => {
+        setShowCookieBanner('none')
+    }
+
+    const cookieBannerStyle = {
+        "display": showCookieBanner
+
+    }
+
     return (
         <div className={"body"}>
             <div className={"body-container"}>
                 <Form/>
             </div>
-            <div className={"notcookie-container"}>
-                <CookieBanner/>
+            <div className={"notcookie-container"} style={cookieBannerStyle}>
+                <CookieBanner onClick={changeVisibility} />
             </div>
         </div>
     )
--- frontend/landingPageSrc/components/CookieBanner.js
+++ frontend/landingPageSrc/components/CookieBanner.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Button from "./Button";
 
-const CookieBanner = () => {
+const CookieBanner = (props) => {
 
     return (
         <div className={"notcookiebanner"}>
             <h3>This website uses cookies. By using this site, you agree to our cookie policy.</h3>
-            <Button classname={"cookiebutton"} name={"I do understand."} onClick={() => console.log("hej")}/>
+            <Button classname={"cookiebutton"} name={"Close"} onClick={props.onClick}/>
         </div>
     )
 }
