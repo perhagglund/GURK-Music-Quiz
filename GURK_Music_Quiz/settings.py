0331e191b1e9f839293cb62c2e082b367d5c3e30
AdamBeijar <56826955+AdamBeijar@users.noreply.github.com>
2021-08-03 12:12:44 +0300

    Added-channels-and-websocket-support

--- GURK_Music_Quiz/settings.py
+++ GURK_Music_Quiz/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'frontend.apps.FrontendConfig',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = "GURK_Music_Quiz.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
