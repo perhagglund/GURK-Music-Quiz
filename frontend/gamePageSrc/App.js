bef3a7e9c47771c563ed4442fa5bebb8c6da72f3
perhagglund <86194556+perhagglund@users.noreply.github.com>
2021-11-08 16:25:35 +0100

    added more lobbyPage style

--- frontend/gamePageSrc/App.js
+++ frontend/gamePageSrc/App.js
@@ -1,11 +1,10 @@
 import React, {useEffect, useState} from "react";
-<<<<<<< Updated upstream
 import { gameclient } from "../lobbyPageSrc/services/gameWebSocketConnect";
 
 const App = () => {
     const session = sessionStorage.getItem("uniqueID")
     const [gameState, setGameState] = useState("Not Online")
-    useEffect(() => {        
+    useEffect(() => {
         gameclient.onopen = () => {
             gameclient.send(JSON.stringify({
                 "ContentType": "checkID",
@@ -26,64 +25,11 @@ const App = () => {
             window.location.href = "/"
         }
     }
-    
+
     return (
         <div>
             {session}
             {gameState}
-=======
-import Players from "../lobbyPageSrc/components/lobbyComponents/Players";
-import CookieBanner from "../landingPageSrc/components/CookieBanner";
-import SongSelector from "./components/SongSelector";
-
-const App = () => {
-
-    const [showCookieBanner, setShowCookieBanner] = useState('none')
-    useEffect(() => {
-        if(!localStorage.getItem("cookieAccepted")){
-            setShowCookieBanner("")
-        }
-    }, [])
-    const changeVisibility = (event) => {
-        event.preventDefault()
-        setShowCookieBanner('none')
-        localStorage.setItem("cookieAccepted", "true")
-    }
-    const cookieBannerStyle = {
-        "display": showCookieBanner
-    }
-    const playerList = [{
-        "nickname": "Doniel",
-        "eyes": 2,
-        "mouth": 1,
-        "color": 0
-    },
-        {
-            "nickname": "Per",
-            "eyes": 1,
-            "mouth": 0,
-            "color": 2
-        },
-        {
-            "nickname": "Adam(Alfred)",
-            "eyes": 1,
-            "mouth": 3,
-            "color": 2
-        }]
-    return (
-        <div>
-            <div className={"body"}>
-                <div className={"body-container"}>
-                    <div className={"main-container"}>
-                        <SongSelector/>
-                        <Players playerList={playerList}/>
-                    </div>
-                </div>
-                <div className={"notcookie-container"} style={cookieBannerStyle}>
-                    <CookieBanner onClick={changeVisibility} />
-                </div>
-            </div>
->>>>>>> Stashed changes
         </div>
     )
 }
--- frontend/lobbyPageSrc/components/lobbyComponents/chatInput.js
+++ frontend/lobbyPageSrc/components/lobbyComponents/chatInput.js
@@ -3,15 +3,9 @@ import Button from "../../../landingPageSrc/components/Button";
 
 const ChatInput = (props) => {
     return (
-<<<<<<< Updated upstream
-        <div>
-            <input placeholder={"Send Message"} onChange={props.handleChange} value={props.value} onKeyPress={props.handleEnterPress}/>
+        <div className={"chat-input"}>
+            <input className={"chat-input-field"} placeholder={"Send Message"} onChange={props.handleChange} value={props.value} onKeyPress={props.handleEnterPress}/>
         {/*<Button classname={""} onClick={props.onSubmit} name={"Submit"} disabled={false}/>*/}
-=======
-        <div className={"chat-input"} >
-            <input className={"chat-input-field"} placeholder={"Send Message"} onChange={props.handleChange} value={props.value}/>
-            <Button classname={"chat-submit-button"} onClick={props.onSubmit} name={"Submit"}/>
->>>>>>> Stashed changes
         </div>
     )
 }
